cac34012bc4b3992f5fa94dde233b9eb812e83af
cham-rumaneh <sham.rumaneh@gmail.com>
2025-08-01 12:02:27 -0700

    my updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <p className='BM_p'>Baraa Masri</p>
       <h1 className='MA-h1'>Mohammad Alkreima</h1>
       <p>Batool Mahmoud</p>
+      <h1 className='SR-h1'>Im Sham</h1>
     </>
   )
 }
